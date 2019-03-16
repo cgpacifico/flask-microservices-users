dabcde0abe168cb492880f40e7f56a9e974d36f8
Cara Pacifico <carapacifico@Caras-MacBook-Pro.local>
2019-03-16 10:37:04 -0400

    APP_SETTINGS from project.config

--- project/__init__.py
+++ project/__init__.py
@@ -4,7 +4,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
+import os
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
+
+# view the app configs, including DEBUG and TESTING
+# $: docker-compose logs -f users-service
+import sys
+print(app.config, file=sys.stderr)
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
